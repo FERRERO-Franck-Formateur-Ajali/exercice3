843799c9fabc9abec0eec21f0cb94055fda09c5c
madbrain67 <ferrerofranck@yahoo.fr>
2021-10-04 12:49:05 +0200

    init

--- article.php
+++ article.php
@@ -1,13 +1,1 @@
 <?php
-
-require_once 'articles.php';
-
-if (array_key_exists($_GET['slug'], $articles)) {
-    echo $articles[$_GET['slug']]['nom'];
-} else {
-    echo 'Désolé le produit n\'existe pas';
-}
-
-echo '<br><a href="/" class="btn btn-dark">home</a>';
-
-//http://localhost:8000/article.php?slug=tee-shirt-s
